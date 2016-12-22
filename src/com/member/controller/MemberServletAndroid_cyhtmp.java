8d71375893d7b3d50a468094f9175fc32c35a578
cyhbench <lovingchao@gmail.com>
2016-12-22 22:04:52 +0800

    1.修改android的c 2.增noimage

--- src/com/member/controller/MemberServletAndroid_cyhtmp.java
+++ src/com/member/controller/MemberServletAndroid_cyhtmp.java
@@ -28,13 +28,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import com.google.gson.stream.JsonReader;
 
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
 
 @SuppressWarnings("serial")
-@WebServlet("/MemberServletAndroid")
-public class MemberServletAndroid extends HttpServlet {
+@WebServlet("/MemberServletAndroid_cyhtmp")
+public class MemberServletAndroid_cyhtmp extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -44,6 +43,19 @@ public class MemberServletAndroid extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
 
+//		req.setCharacterEncoding("UTF-8");
+//		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+//		BufferedReader br = req.getReader();
+//		StringBuffer jsonIn = new StringBuffer();
+//		String line = null;
+//		while((line = br.readLine()) != null){
+//			jsonIn.append(line);
+//		}
+//		JsonObject jsonObject = gson.fromJson(jsonIn.toString(), JsonObject.class);
+//		MemberService memberSvc = new MemberService();
+//		String action = jsonObject.get("action").getAsString();
+//		System.out.println("action:" +action);
+//		System.out.println("jsonIn.toString():" +jsonIn.toString());
 		req.setCharacterEncoding("UTF-8");
 		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		MemberService memberSvc = new MemberService();
@@ -66,20 +78,27 @@ public class MemberServletAndroid extends HttpServlet {
 			List<MemberVO> memberlist = memberSvc.getAll();
 			SendResponse.writeText(res, gson.toJson(memberlist));
 			return;
+//			List<MemberVO> memberlist = memberSvc.getAll();
+//			outStr.append(gson.toJson(memberlist));
+//			SendResponse.writeText(res, outStr.toString());
+//			return;
 		}
 
 		
 		if ("getOne_For_Display".equals(action)) {
 			
-			String mem_noJson = jsonObject.get("mem_no").getAsString();
-			JsonReader reader = new JsonReader(new StringReader(mem_noJson));
-			reader.setLenient(true);
-			String mem_no = gson.fromJson(reader, String.class);
-//			MemberVO memberVO = memberSvc.getOneMemberTextOnly(mem_no);
-			MemberVO memberVO = memberSvc.getOneMember(mem_no); 
+//			String memnoJson = jsonObject.get("memno").getAsString();
+//			JsonReader reader = new JsonReader(new StringReader(memnoJson));
+//			reader.setLenient(true);
+//			String memno = gson.fromJson(reader, String.class);
+//			MemberVO memberVO = memberSvc.getOneMemberTextOnly(memno);
+//			
+//			outStr.append(gson.toJson(memberVO));
+//			SendResponse.writeText(res, outStr.toString());
 			
-			outStr.append(gson.toJson(memberVO));
-			SendResponse.writeText(res, outStr.toString());	
+			MemberVO member = gson.fromJson(jsonObject.get("memberVO").getAsString(), MemberVO.class);
+			memberSvc.deleteMember(member.getMem_no());
+			SendResponse.writeText(res, String.valueOf(member));
 		}
 		
 		if ("getOne_For_Update".equals(action)) { 
@@ -91,10 +110,7 @@ public class MemberServletAndroid extends HttpServlet {
 		if ("insert".equals(action)) {   
 		}
 			
-		if ("delete".equals(action)) {
-			MemberVO member = gson.fromJson(jsonObject.get("memberVO").getAsString(), MemberVO.class);
-			memberSvc.deleteMember(member.getMem_no());
-			SendResponse.writeText(res, String.valueOf(member));
+		if ("delete".equals(action)) { 
 		}
 		if ("getImage".equals(action)) {
 			OutputStream os = res.getOutputStream();
@@ -109,7 +125,7 @@ public class MemberServletAndroid extends HttpServlet {
 				res.setContentType("image/jpeg");
 				res.setContentLength(mem_image.length);
 			} else {
-				InputStream in = getServletContext().getResourceAsStream("/noImages/noimage.jpg");
+				InputStream in = getServletContext().getResourceAsStream("/NoData/nopic.jpg");
 				mem_image = new byte[in.available()];
 				in.read(mem_image);
 				in.close();
