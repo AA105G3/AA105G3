b6937a879139e553a8e03f44e78e655386d12b6e
C1600478 <stanley4088@gmail.com>
2016-12-22 16:47:57 +0800

    新增過濾器

--- src/com/front/login/FLoginHandler.java
+++ src/com/front/login/FLoginHandler.java
@@ -56,7 +56,7 @@ public class FLoginHandler extends HttpServlet {
 		if (!allowUser(mem_ac, mem_pw)) {          //【帳號 , 密碼無效時】
 			out.println("<HTML><HEAD><TITLE>Access Denied</TITLE></HEAD>");
 			out.println("<BODY>你的帳號 , 密碼無效!<BR>");
-			out.println("請按此重新登入 <A HREF="+req.getContextPath()+"/front-end/Flogin.html>重新登入</A>");
+			out.println("請按此重新登入 <A HREF="+req.getContextPath()+"/front-end/Flogin/Flogin.html>重新登入</A>");
 			out.println("</BODY></HTML>");
 		}
 		else {                                       //【帳號 , 密碼有效時, 才做以下工作】
@@ -72,7 +72,7 @@ public class FLoginHandler extends HttpServlet {
 				}
 			}catch (Exception ignored) {}
 
-			res.sendRedirect(req.getContextPath()+"/front-end/Flogin_success.jsp");  //*工作3: (-->如無來源網頁:則重導至login_success.jsp)
+			res.sendRedirect(req.getContextPath()+"/front-end/Flogin/Flogin_success.jsp");  //*工作3: (-->如無來源網頁:則重導至login_success.jsp)
 		}
 	}
 
